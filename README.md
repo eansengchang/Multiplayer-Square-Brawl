7db2f67da69d41516484f9f625bd216b35f5b9bd
eansengchang <76442284+eansengchang@users.noreply.github.com>
2022-11-25 00:46:15 +0000

    first commit

--- README.md
+++ README.md
@@ -1,1 +1,2 @@
 # Multiplayer-Square-Brawl
+# Poker-Multiplayer
--- server/server.js
+++ server/server.js
@@ -1,211 +1,162 @@
-const io = require("socket.io")({
-    cors: {
-        origin: '*',
-        methods: ["GET", "POST"]
-    }
+const io = require('socket.io')({
+  cors: {
+    origin: '*',
+    methods: ['GET', 'POST'],
+  },
 });
 
-const { initGame, gameLoop } = require("./game");
-const { FRAME_RATE, GAME_WIDTH, GAME_HEIGHT, BOUNDARY_WIDTH, GRAVITY, DELTATIME } = require("./constants");
-const { makeid } = require("./utils");
+const { Game, initGame } = require('./game');
 
-const Matter = require('matter-js');
-const { Engine, Render, Runner, Body, Events, Composite, Composites, Common, Mouse, Bodies, Collision } = require('matter-js');
-const Boundary = require('./boundary');
-const Player = require('./player');
+const { makeid } = require('./utils');
 
+//socketroom is a dictionary that maps socket id to room names
 const socketRooms = {};
 
 /*
 game is a dictionary with the keys as the roomNumbers
-    state: the payload being sent and information on stuff
-    engine: the engine of the game
-    players: the dictionary of players mapped by their player numbers
-    bullets: the list of bullets 
-    boundaries: the list of boundaries
+    players: {},
+    hands: {},
+    table: [],
+    startingTurn: null,
+    turn: null,
+    turnArray: null,
+    roundPlaying: false,
+    gameStarted: false,
+    gameStage: 0,
 */
-const game = {}
+const game = {};
 
-io.on('connect', socket => {
+io.on('connect', (socket) => {
+  socket.on('actionDone', handleAction);
+  socket.on('newRound', handleNewRound);
+  socket.on('newGame', handleNewGame);
+  socket.on('joinGame', handleJoinGame);
+  socket.on('disconnect', handleDisconnect);
 
-    socket.on('keyDown', handleKeyDown);
-    socket.on('keyUp', handleKeyUp);
-    socket.on('newGame', handleNewGame);
-    socket.on('joinGame', handleJoinGame);
-    socket.on('disconnect', handleDisconnect)
+  function handleAction(action) {
+    const roomCode = socketRooms[socket.id];
+    let gameInstance = game[roomCode];
 
-    function handleDisconnect() {
-        let roomName = socketRooms[socket.id];
-        if (!roomName) return;
+    //action of person not in game
+    if (!roomCode) return;
 
-        //leave player from game
-        delete socketRooms[socket.id]
-        let playerId = socket.number
-        //delete state players and game players
-        delete game[roomName].state.players[playerId];
-        delete game[roomName].players[playerId];
+    //game not started
+    if (!gameInstance.gameStarted) return;
 
-        //if no one in room
-        if (!io.sockets.adapter.rooms.get(roomName)) {
-            clearInterval(game[roomName].intervalId)
-            delete game[roomName]
-        }
+    //game just finished
+    if (gameInstance.gameStage == 4) return;
+
+    //not person's turn
+    if (gameInstance.turnArray[gameInstance.turn] != socket.playerName) {
+      return;
     }
 
-    function handleJoinGame(roomName) {
-        if (!roomName) {
-            socket.emit('unknownCode');
-            return
-        }
-        //room is a set
-        const room = io.sockets.adapter.rooms.get(roomName);
+    //TODO handle action then move next round
+    gameInstance.doAction(socket.playerName, action);
 
-        //get the size of the room if its a thing
-        let numsockets;
-        if (room) {
-            numsockets = room.size
-        }
+    emitGameState(roomCode, gameInstance);
 
-        if (!room || numsockets === 0) {
-            socket.emit('unknownCode');
-            return
-        } else if (numsockets >= 4) {
-            socket.emit('tooManyPlayers');
-            return
-        }
+    //game ended
+    if (gameInstance.gameStage == 4 || !gameInstance.roundPlaying) {
+      //emit game state to show remaining cards
+      gameInstance.gameStage = 4;
+      emitGameState(roomCode, gameInstance);
 
-        socketRooms[socket.id] = roomName;
-        socket.join(roomName);
+      emitWinner(roomCode, {
+        winner: gameInstance.winner,
+        handRanks: gameInstance.handRanks,
+      });
+    }
+  }
 
-        //to get socket number we loop through to get the highest socket nunber and add one
+  //another hand is dealt
+  function handleNewRound() {
+    const roomCode = socketRooms[socket.id];
 
-        //create random coords
-        let x = Math.random() * GAME_WIDTH;
-        let y = Math.random() * GAME_HEIGHT;
-        let hue = Math.floor(Math.random() * 360);
+    game[roomCode].newRound();
 
-        //find the lowest free player id
-        let playerId = 1;
-        while (game[roomName].players[playerId] != null) {
-            playerId += 1;
-        }
+    emitGameState(roomCode, game[roomCode]);
+  }
 
-        //add the players to the world, both the matter world and the state object
-        game[roomName].players[playerId] = (new Player(x, y, hue, game[roomName].engine.world, playerId))
-        game[roomName].state.players[playerId] = {
-            pos: {
-                x: x,
-                y: y
-            },
-            heading: 0,
-            angle: 0,
-            hue: hue,
-            health: 100,
-        }
+  function handleNewGame(firstPlayerName) {
+    let roomCode = makeid(5);
+    socketRooms[socket.id] = roomCode;
+    socket.emit('gameCode', roomCode);
 
-        socket.number = playerId;
-        socket.emit('init', playerId);
-        socket.emit("gameCode", roomName)
+    socket.join(roomCode);
+    socket.playerName = firstPlayerName;
+    socket.emit('init');
+    startNewGame(roomCode, firstPlayerName);
+  }
+
+  function startNewGame(roomCode, firstPlayerName) {
+    //game stuff for easy use
+    game[roomCode] = initGame(firstPlayerName);
+    game[roomCode].gameStarted = true;
+  }
+
+  function handleJoinGame({ roomCode, playerName }) {
+    if (!roomCode) {
+      socket.emit('unknownCode');
+      return;
+    }
+    //room is a set
+    const room = io.sockets.adapter.rooms.get(roomCode);
+
+    //get the size of the room if it exists
+    let numsockets;
+    if (room) {
+      numsockets = room.size;
     }
 
-    function handleNewGame() {
-        let roomName = makeid(5);
-        socketRooms[socket.id] = roomName;
-        socket.emit("gameCode", roomName)
-
-        socket.join(roomName);
-        socket.number = 1;
-        socket.emit("init", 1)
-        startNewGame(roomName);
+    if (!room || numsockets === 0) {
+      socket.emit('unknownCode');
+      return;
+    } else if (
+      Object.keys(game[roomCode].players).includes(playerName) ||
+      Object.keys(game[roomCode].joiningPlayers).includes(playerName)
+    ) {
+      socket.emit('sameName');
+      return;
     }
 
-    function handleKeyDown(keyCode) {
-        const roomName = socketRooms[socket.id]
+    socketRooms[socket.id] = roomCode;
+    socket.join(roomCode);
 
-        if (!roomName) {
-            return;
-        }
+    //add the players to the state object
+    game[roomCode].addPlayer(playerName);
 
-        try {
-            keyCode = parseInt(keyCode)
-        } catch (error) {
-            console.log(error);
-            return
-        }
+    socket.playerName = playerName;
+    socket.emit('gameCode', roomCode);
 
-        if (game[roomName].state.gameStarted) {
-            game[roomName].players[socket.number].keyDown(keyCode)
-        }
+    if (game[roomCode].roundPlaying) emitGameState(roomCode, game[roomCode]);
+  }
+
+  function handleDisconnect() {
+    let roomCode = socketRooms[socket.id];
+    if (!roomCode) return;
+
+    //leave player from game
+    delete socketRooms[socket.id];
+    let playerName = socket.playerName;
+    //delete state players and game players
+    delete game[roomCode].players[playerName];
+
+    //if no one in room
+    if (!io.sockets.adapter.rooms.get(roomCode)) {
+      delete game[roomCode];
     }
+  }
+});
 
-    function handleKeyUp(keyCode) {
-        const roomName = socketRooms[socket.id]
+function emitGameState(roomCode, gameInstance) {
+  //TODO gameObj to state
 
-        if (!roomName) {
-            return;
-        }
-
-        try {
-            keyCode = parseInt(keyCode)
-        } catch (error) {
-            console.log(error);
-            return
-        }
-        if (game[roomName].state.gameStarted) {
-            game[roomName].players[socket.number].keyUp(keyCode)
-        }
-    }
-
-    function startNewGame(roomName) {
-        //game stuff for easy use
-        game[roomName] = {
-            state: initGame(),
-            engine: Engine.create(),
-            players: undefined,
-            boundaries: [],
-            bullets: [],
-            intervalId: undefined
-        }
-        game[roomName].state.gameStarted = true;
-
-        let engine = game[roomName].engine
-        engine.gravity.y = GRAVITY;
-
-        //get the hue from newly created state (first player's hue)
-        let hue = game[roomName].state.players[1].hue
-        game[roomName].players = { 1: new Player(300, 300, hue, engine.world, 1) };
-
-        game[roomName].boundaries.push(new Boundary(GAME_WIDTH / 2, GAME_HEIGHT, GAME_WIDTH, BOUNDARY_WIDTH, engine.world))
-        game[roomName].boundaries.push(new Boundary(GAME_WIDTH / 2, 0, GAME_WIDTH, BOUNDARY_WIDTH, engine.world))
-        game[roomName].boundaries.push(new Boundary(0, GAME_HEIGHT / 2, BOUNDARY_WIDTH, GAME_HEIGHT, engine.world))
-        game[roomName].boundaries.push(new Boundary(GAME_WIDTH, GAME_HEIGHT / 2, BOUNDARY_WIDTH, GAME_HEIGHT, engine.world))
-        game[roomName].boundaries.push(new Boundary(GAME_WIDTH / 2, GAME_HEIGHT / 3, 400, 20, engine.world))
-        game[roomName].boundaries.push(new Boundary(GAME_WIDTH / 2, GAME_HEIGHT * 2 / 3, 400, 20, engine.world))
-        game[roomName].boundaries.push(new Boundary(GAME_WIDTH / 2, GAME_HEIGHT - 50, 20, 100, engine.world))
-
-
-        game[roomName].intervalId = setInterval(() => {
-            const winner = gameLoop(game[roomName]);
-
-            if (!winner) {
-                emitGameState(roomName, game[roomName].state)
-            } else {
-                emitGameOver(roomName, winner);
-                game[roomName] = null;
-
-                socketRooms[socket.id] = null;
-            }
-
-        }, DELTATIME);
-    }
-})
-
-function emitGameState(roomName, state) {
-    io.sockets.in(roomName).emit('gameState', JSON.stringify(state))
+  io.sockets.in(roomCode).emit('gameState', gameInstance.getObj());
 }
 
-function emitGameOver(roomName, winner) {
-    io.sockets.in(roomName).emit('gameOver', JSON.stringify({ winner }))
+function emitWinner(roomCode, obj) {
+  io.sockets.in(roomCode).emit('winner', obj);
 }
 
-io.listen(process.env.PORT || 3000);
+io.listen(process.env.PORT || 9321);
